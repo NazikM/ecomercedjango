b663b98d148ea31675de778e8dd8236d046a191c
Nazar Marusyn <marusinnazar@gmail.com>
2022-05-06 23:54:29 +0300

    Add ImageField to models.py

--- ecommerce_site/urls.py
+++ ecommerce_site/urls.py
@@ -1,3 +1,6 @@
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.contrib import admin
 from django.urls import path, include
 
@@ -5,3 +8,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls'))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- store/models.py
+++ store/models.py
@@ -20,6 +20,14 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except NotImplementedError:
+            url = ''
+        return url
+
 
 class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
@@ -49,4 +57,3 @@ class ShippingAddress(models.Model):
 
     def __str__(self):
         return self.address
-
