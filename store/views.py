afaf52f53026b912cdbd7297a5d8b0ee169400a7
Nazar Marusyn <marusinnazar@gmail.com>
2022-05-06 23:13:51 +0300

    Initial commit

--- store/views.py
+++ store/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 
+from store.models import Product
+
 
 def store(request):
-    context = {}
+    products = Product.objects.all()
+    context = {"products": products}
     return render(request, "store/Store.html", context=context)
 
 
